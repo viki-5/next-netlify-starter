de369e03217394276b9cd3e44e8c4c570e771e58
Vishnu S <101126132+viki-5@users.noreply.github.com>
2023-10-27 14:36:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container" style={{ backgroundImage: "url('/images/background.jpg')" }}>
+    <div className="container" style={{ backgroundImage: "url('public/download.jpg')" }}>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
