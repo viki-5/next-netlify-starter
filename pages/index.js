322ede500ac9bf112f940e3187c9df7a672fa56a
Vishnu S <101126132+viki-5@users.noreply.github.com>
2023-10-27 14:39:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Progress Page" />
 
         {/* Your HTML table with CSS code goes here */}
         <form>
