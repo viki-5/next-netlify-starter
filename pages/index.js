f2949992f4bea3aaa9d0fdf39cf3bdd24798b6d2
Vishnu S <101126132+viki-5@users.noreply.github.com>
2023-10-27 14:28:17 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,14 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container" style={{ backgroundImage: "url()" }}>
+    <div className="container" style={{ backgroundImage: "url('download.jpg')" }}>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Progress Page" />
 
         {/* Your HTML table with CSS code goes here */}
         <form>
